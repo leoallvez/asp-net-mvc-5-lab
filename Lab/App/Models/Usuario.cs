1f21884056785f88bb35eaf01f6d6f365024a1aa
leoallvez <leoallvez@hotmail.com>
2017-12-30 00:34:15 -0200

    working with validations

--- Lab/App/Models/Usuario.cs
+++ Lab/App/Models/Usuario.cs
@@ -2,17 +2,26 @@
 using System.Web;
 using System.Linq;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Models
 {
     public class Usuario
     {
         public string Nome { get; set; }
+
+        [Display(Name = "Observações")]
         public string Observacoes { get; set; }
+
         public int Idade { get; set; }
+
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+
         public string Login { get; set; }
+
         public string Senha { get; set; }
+
         public string Confirmar { get; set; }
     }
 }
