fd9096a86fcc8c4a5efb0c7dc9ebabdecdcddf38
leoallvez <leoallvez@hotmail.com>
2018-01-06 01:05:37 -0200

    Materialize application

--- Lab/Materialize/App_Start/BundleConfig.cs
+++ Lab/Materialize/App_Start/BundleConfig.cs
@@ -8,24 +8,21 @@ namespace Materialize
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = true;
+            //Custom bundles.
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include("~/Scripts/*.js"));
+            //Order of the script bundles call.
+            var ordemScript = new BundleFileSetOrdering("meuScript");
+            ordemScript.Files.Add("jquery-3.2.1.min.js");
+            ordemScript.Files.Add("materialize.min.js");
+            bundles.FileSetOrderList.Insert(0, ordemScript);
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+            //Order of the script bundles call.
+            var ordemCss = new BundleFileSetOrdering("meuScript");
+            ordemCss.Files.Add("materialize.min.css");
+            ordemCss.Files.Add("comum.css");
+            bundles.FileSetOrderList.Insert(0, ordemCss);
         }
     }
 }
